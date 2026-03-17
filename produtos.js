d2ac4d511718cf7fa712e7a771247a8ba9316ca1
Harleyzinn <batatapirada32@gmail.com>
2026-03-17 18:50:42 -0300

    Create produtos.js

---
+++ produtos.js
@@ -0,0 +1,24 @@
+// Funciona como nosso banco de dados. Adicione novos objetos aqui para expandir o cardápio.
+const produtosData = [
+    {
+        id: 'ovo-tradicional',
+        nome: 'Ovo Tradicional',
+        preco: 40.00,
+        desc: 'Ao leite ou Branco. Com bombons. (350g)',
+        img: 'https://images.unsplash.com/photo-1522814890230-0eb5379ce85f?auto=format&fit=crop&w=150&q=80'
+    },
+    {
+        id: 'mini-ovos',
+        nome: 'Mini Ovos de Colher',
+        preco: 55.00,
+        desc: 'Duo recheado com Brownie e Maracujá. (100g)',
+        img: 'https://images.unsplash.com/photo-1616035118476-0f30c6fa4d52?auto=format&fit=crop&w=150&q=80'
+    },
+    {
+        id: 'ovo-panda',
+        nome: 'Ovo Panda Kids',
+        preco: 60.00,
+        desc: 'Casca ao leite com confeito colorido. (100g)',
+        img: 'https://images.unsplash.com/photo-1554181467-961fba293ee6?auto=format&fit=crop&w=150&q=80'
+    }
+];
